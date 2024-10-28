7e9abe236fbe9cc1e6d2ad9eb86146e12edfce01
Max Unanian <Zeptorqyiad99@gmail.com>
2024-10-28 19:30:37 +0500

    Working with geoBlock

--- js/geo.js
+++ js/geo.js
@@ -1,16 +1,38 @@
+const geoBlock = document.querySelector('.geo-block')
+const geoPanel = document.querySelector('.geo__list-item')
+const geoCloseIcon = document.querySelector('.close-icon')
+const searchCityPanel = document.getElementById('searchPanel')
+
+geoBlock.addEventListener('click', openPanel)
+
+function openPanel() {
+   geoPanel.classList.toggle('active')
+   document.body.classList.toggle('_lock')
+}
+
+geoCloseIcon.onclick = () => {
+   if (geoPanel.classList.contains('active')) {
+      geoPanel.classList.remove('active')
+      document.body.classList.remove('_lock')
+   }
+}
+searchCityPanel.addEventListener('click', () => {
+   searchCityPanel.style.minWidth = '100%'
+   geoCloseIcon.style.display = 'none'
+})
+
+// searchCityPanel.onfocus = () => {}
+
 navigator.geolocation.getCurrentPosition(success, error, {
-   // высокая точность
    enableHighAccuracy: true,
 })
 
 function success({ coords }) {
-   // получаем широту и долготу
    const { latitude, longitude } = coords
    const position = [latitude, longitude]
-   console.log(position) // [широта, долгота]
+   console.log(position)
 }
 
 function error({ message }) {
    console.log(message) // при отказе в доступе получаем PositionError: User denied Geolocation
 }
-
